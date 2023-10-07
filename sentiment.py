3a5c7dccc85b6c84d584aabf3a67c340b653f0a9
kairung <kairung@webmail.npru.ac.th>
2023-10-07 21:06:44 +0700

    .commit

--- sentiment.py
+++ sentiment.py
@@ -5,6 +5,10 @@ from pythainlp import word_tokenize
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib as plt
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
 column_names=["text"]
 # Add header row while reading a CSV file
 df = pd.read_csv('/content/test.csv', names=column_names)
@@ -29,3 +33,53 @@ dftext['text_tokens'] = dftext[0].apply(text_process)
 
 df_pos = dftext[dftext[1] == 'pos']
 pos_word_all = " ".join(text for text in df_pos['text_tokens'])
+
+mpl.font_manager.fontManager.addfont('THSarabunChula-Regular.ttf')
+reg = r"[ก-๙a-zA-Z']+"
+fp = 'THSarabunChula-Regular.ttf'
+wordcloud = WordCloud(stopwords=thai_stopwords, background_color = 'white', max_words=2000, height = 2000, width=4000, font_path=fp, regexp=reg).generate(pos_word_all)
+mpl.figure.Figure(figsize = (30,8))
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
+
+df_neg = dftext[dftext[1] == 'neg']
+neg_word_all = " ".join(text for text in df_neg['text_tokens'])
+wordcloud = WordCloud(stopwords=thai_stopwords, background_color = 'white', max_words=2000, height = 2000, width=4000, font_path=fp, regexp=reg).generate(neg_word_all)
+mpl.figure.Figure(figsize = (30,8))
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
+
+from sklearn.model_selection import train_test_split
+X = dftext[['text_tokens']]
+y = dftext[1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+from sklearn.feature_extraction.text import CountVectorizer
+cvec = CountVectorizer(analyzer=lambda x:x.split(' '))
+cvec.fit_transform(X_train['text_tokens'])
+cvec.vocabulary_
+
+train_bow = cvec.transform(X_train['text_tokens'])
+pd.DataFrame(train_bow.toarray(), columns=cvec.get_feature_names_out(), index=X_train['text_tokens'])
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(train_bow, y_train)
+
+from sklearn.metrics import confusion_matrix,classification_report
+test_bow = cvec.transform(X_test['text_tokens'])
+test_predictions = lr.predict(test_bow)
+print(classification_report(test_predictions, y_test))
+
+my_text = 'ตรงปกส่งไวครับ'
+my_tokens = text_process(my_text)
+my_bow = cvec.transform(pd.Series([my_tokens]))
+my_predictions = lr.predict(my_bow)
+my_predictions
+
+
+
+
+
