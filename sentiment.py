e29b3c82d874d7a15609eeeaced37cb8266d64d0
kairung <kairung@webmail.npru.ac.th>
2023-10-07 22:58:41 +0700

    .commit

--- sentiment.py
+++ sentiment.py
@@ -57,20 +57,37 @@ plt.axis('off')
 st.pyplot(fg2)
 plt.show()
 
+from sklearn.model_selection import train_test_split
+X = dftext[['text_tokens']]
+y = dftext[1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
 from sklearn.feature_extraction.text import CountVectorizer
 cvec = CountVectorizer(analyzer=lambda x:x.split(' '))
+cvec.fit_transform(X_train['text_tokens'])
+#cvec.vocabulary_
+
+train_bow = cvec.transform(X_train['text_tokens'])
+pd.DataFrame(train_bow.toarray(), columns=cvec.get_feature_names_out(), index=X_train['text_tokens'])
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(train_bow, y_train)
+
+from sklearn.metrics import confusion_matrix,classification_report
+test_bow = cvec.transform(X_test['text_tokens'])
+test_predictions = lr.predict(test_bow)
+#print(classification_report(test_predictions, y_test))
 
 
 my_text=st.text_area("กรุณาป้อนความคิดเห็นสำหรับใช้ในการวิเคราะห์")
 
 if st.button("วิเคราะห์ความคิดเห็น"):
-    filename = 'lr_model.pkl'
-    loaded_model = pickle.load(open(filename, 'rb'))
-
     my_tokens = text_process(my_text)
     my_bow = cvec.transform(pd.Series([my_tokens]))
-    my_predictions = loaded_model.predict(my_bow)
+    my_predictions = lr.predict(my_bow)
     my_predictions  
+    
     st.button("ไม่วิเคราะห์ความคิดเห็น")
 else:
     st.button("ไม่วิเคราะห์ความคิดเห็น")
