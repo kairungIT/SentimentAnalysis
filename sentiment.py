8e222dbf3eb5417c62e9f0c8e0c3e84ae05b474e
kairung <kairung@webmail.npru.ac.th>
2023-10-07 21:23:49 +0700

    .commit

--- sentiment.py
+++ sentiment.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import pandas as pd
 import pythainlp
 from pythainlp.corpus.common import thai_stopwords
@@ -5,21 +6,19 @@ from pythainlp import word_tokenize
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib as plt
 
-import matplotlib as mpl
+
 import matplotlib.pyplot as plt
 
 
 column_names=["text"]
 # Add header row while reading a CSV file
-df = pd.read_csv('/content/test.csv', names=column_names)
+df = pd.read_csv('./test.csv', names=column_names)
 df2=df['text'].str.split(pat="\t",expand=True)
 dftext=pd.DataFrame(df2)
 dftext=dftext.iloc[:,0:2]
-dftext[1].value_counts().plot.bar()
-
-
+st.bar_chart(dftext[1].value_counts())
 thai_stopwords = list(thai_stopwords())
-thai_stopwords
+st.write(thai_stopwords)
 
 def text_process(text):
     final = "".join(u for u in text if u not in ("?", ".", ";", ":", "!", '"', "ๆ", "ฯ"))
@@ -34,11 +33,12 @@ dftext['text_tokens'] = dftext[0].apply(text_process)
 df_pos = dftext[dftext[1] == 'pos']
 pos_word_all = " ".join(text for text in df_pos['text_tokens'])
 
-mpl.font_manager.fontManager.addfont('THSarabunChula-Regular.ttf')
+#import matplotlib as mpl
+plt.font_manager.fontManager.addfont('THSarabunChula-Regular.ttf')
 reg = r"[ก-๙a-zA-Z']+"
 fp = 'THSarabunChula-Regular.ttf'
 wordcloud = WordCloud(stopwords=thai_stopwords, background_color = 'white', max_words=2000, height = 2000, width=4000, font_path=fp, regexp=reg).generate(pos_word_all)
-mpl.figure.Figure(figsize = (30,8))
+plt.figure.Figure(figsize = (30,8))
 plt.imshow(wordcloud)
 plt.axis('off')
 plt.show()
