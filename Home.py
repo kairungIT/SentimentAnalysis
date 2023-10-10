23ebf0394bdf3e3cc72b1f74dd57bd436b815907
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-10-10 15:01:21 +0700

    .commit

--- Home.py
+++ Home.py
@@ -3,7 +3,6 @@ import time
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
-from streamlit_lottie import st_lottie_spinner
 
 def load_lottieurl(url: str):
     r = requests.get(url)
@@ -11,12 +10,12 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-
 st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
 st.subheader("Kairung Hengpraprohm")
 
 col1, col2 = st.columns(2)
 with col1:
+    st.image('./pic/kairung.jpg')
     lot3="https://lottie.host/defef63f-5b59-455d-a6e0-ff03f2791104/t43d8wbLKq.json"
     lottie3 = load_lottieurl(lot3)
     st_lottie(lottie3)
