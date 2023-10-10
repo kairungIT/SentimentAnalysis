e575603058f088826f885e150882b00d6cb08cbe
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-10-10 11:14:00 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,7 +1,6 @@
 import json
 import time
 import requests
-
 import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
@@ -12,18 +11,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
-lottie_url_download = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
 lot3="https://lottie.host/2e099151-4397-40ad-ae7b-249bbaf1e8df/gluEhZplO8.json"
-lottie_hello = load_lottieurl(lottie_url_hello)
-lottie_download = load_lottieurl(lottie_url_download)
 lottie3 = load_lottieurl(lot3)
-
-
-st_lottie(lottie_hello, key="hello")
 st_lottie(lottie3)
-
-if st.button("Download"):
-    with st_lottie_spinner(lottie_download, key="download"):
-        time.sleep(5)
-    st.balloons()
+st.balloons()
